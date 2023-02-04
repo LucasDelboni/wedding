75194051d15d253c77261c310f3ace5ee9b12bc2
lucas delboni <lucas.delboni@lianlian.com.br>
2023-02-04 14:48:13 -0300

    removing unused things

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import History from './sections/History';
 import Countdown from './sections/Countdown';
 import Cerimony from './sections/Cerimony';
+import Reception from './sections/Reception';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -11,23 +12,10 @@ import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <History/>
       <Countdown/>
       <Cerimony/>
+      <Reception/>
     </div>
   );
 }
