8cd4bd8ef7373d368519bace192a2c2a67eddb18
lucas delboni <lucas.delboni@lianlian.com.br>
2023-02-05 12:44:31 -0300

    adicionadno informacoes sobre recepcao

--- src/sections/Cerimony.jsx
+++ src/sections/Cerimony.jsx
@@ -7,8 +7,8 @@ export default function Cerimony(){
         <h1>Cerimônia</h1>
         <p>Gostaríamos muito de contar com a presença de todos vocês para rezer por nossa felicidade!</p>
         <p>A cerimônia será rápida e tentaremos ser extremamente pontuais. Contamos com vocês!</p>
-        <p style={{fontWeight: 700}}>Dia 7 de outubro de 2023.</p>
-        <p style={{fontWeight: 700}}>Rua Treze de Maio, 478 - Bela Vista, São Paulo - SP, 01327-000</p>
+        <p><b>Dia 7 de outubro de 2023 na Paróquia Nossa Senhora Achiropita</b></p>
+        <p><b>Rua Treze de Maio, 478 - Bela Vista, São Paulo - SP, 01327-000</b></p>
         <AddressMap 
           title="Cerimonia - Paroquia Achiropita" 
           url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.246645972133!2d-46.64893698446621!3d-23.55739628468502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59b6aa35b411%3A0xe29db2eacb26cb77!2sPar%C3%B3quia%20Nossa%20Senhora%20Achiropita!5e0!3m2!1spt-BR!2sbr!4v1675532048120!5m2!1spt-BR!2sbr"
--- src/sections/Reception.jsx
+++ src/sections/Reception.jsx
@@ -5,6 +5,8 @@ export default function Reception(){
   return (
     <div>
         <h1>Recepção</h1>
+        <p>Os noivos convidam para recepção no dia 07 de outubro de 2023, na <b>Espaço Bonjardim</b>, em São Paulo. Não vai perder, né?</p>
+        <p><b>R. Padre Estevão Pernet, 276 - Tatuapé, São Paulo - SP, 03315-000</b></p>
         <AddressMap title="Recepção" url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14630.818977306324!2d-46.5722102!3d-23.5431196!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcb635fd14accfeaa!2sEspa%C3%A7o%20Bonjardim!5e0!3m2!1spt-BR!2sbr!4v1675532681335!5m2!1spt-BR!2sbr"/>
     </div>
   );
