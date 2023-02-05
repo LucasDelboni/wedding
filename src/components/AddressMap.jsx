c50444381d4477a14661ca1d9ee4a3a6be9ee0cf
lucas delboni <lucas.delboni@lianlian.com.br>
2023-02-05 12:24:41 -0300

    adding more history

--- src/components/AddressMap.jsx
+++ src/components/AddressMap.jsx
@@ -1,7 +1,14 @@
 export default function AddressMap({url, title}){
     return (
-        <div className="google-map-code">
-            <iframe src={url} title={title} width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+        <iframe 
+            src={url}
+            height="100%"
+            width="100%"
+            title={title}
+            style={{border: 0}}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+        />
     );
 }
