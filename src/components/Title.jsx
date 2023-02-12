5f84e7e989fcb864c31fa39e9745b9f2f780fbf2
lucas delboni <lucas.delboni@lianlian.com.br>
2023-02-11 22:19:00 -0300

    colocando estilo dentro do componente

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,16 @@
 export default function Title({title}) {
     return (
-        <div style={{marginBottom: '3%'}}>
-            <h1>{title}</h1>
+        <div style={{marginBottom: '3%', textAlign: 'center'}}>
+            <h1 style={titleStyle}>{title}</h1>
             <img src="floral-titulo.png" alt="arranjo floral"></img>
         </div>
     )
+}
+
+const titleStyle = {
+    marginTop: '5%',
+    marginBottom: '0px',
+    fontFamily: '"Great Vibes", cursive',
+    fontSize: '3em',
+    fontWeight: '500',
 }
