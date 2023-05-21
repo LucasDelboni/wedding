40f9500e0014351195e70c63fb4d57959c3389fa
Adriana Oliveira <adrianaoliveira@MacBook-Pro-de-Adriana.local>
2023-05-21 16:01:03 -0300

    Change page background color

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 import Container from '@mui/material/Container';
 
 export default function Section({children, even}) {
-    let backgroundColor = even ? '#97917A' : '#fcd8b4'
+    let backgroundColor = even ? '#97917A' : '#FCF9F7'
     return (
         <Container style={{ background: backgroundColor }} paddingY={8}>
             {children}
